9edb8fa314021ff934fdcfffd0ff7845edd02eb3
AIT-MOH-Youness <Youness.aitmoh.ensaj@gmail.com>
2026-02-06 22:58:32 +0100

    fixes

--- ci/40_build_docker_image.sh
+++ ci/40_build_docker_image.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 : "${IMAGE_NAME:?IMAGE_NAME missing}"
 : "${IMAGE_TAG:?IMAGE_TAG missing}"
 
-docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" ./ocr-container
 docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
